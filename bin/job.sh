4d0f2b3a60d53d71f8eea09ade3ace3057f35cce
Pascal Simon <simon@puzzle.ch>
2017-06-07 15:00:22 +0200

    make backup job multidb ready

--- bin/job.sh
+++ bin/job.sh
@@ -1,24 +1,38 @@
-#!/bin/sh
-DATE=$(date +%Y-%m-%d-%H-%M)
-dump=$(mysqldump --user=$MYSQL_USER --password=$MYSQL_PASSWORD --host=$MYSQL_SERVICE_HOST $MYSQL_DATABASE)
+#!/bin/bash
 
-if [ $? -ne 0 ]; then
+DATE=$(date +%Y-%m-%d-%H-%M)
+
+function backup_db {
+  DB=$1
+  BACKUP_DIR="$BACKUP_DATA_DIR/$DB"
+
+  mkdir -p $BACKUP_DIR
+  dump=$(mysqldump --user=$MYSQL_ROOT_USER --password=$MYSQL_ROOT_PASSWORD --host=$MYSQL_SERVICE_HOST $DB)
+
+  if [ $? -ne 0 ]; then
     echo "mysqldump not successful: ${DATE}"
     exit 1
-fi
+  fi
 
-printf '%s' "$dump" | gzip > $BACKUP_DATA_DIR/dump-${DATE}.sql.gz
+  printf '%s' "$dump" | gzip > $BACKUP_DIR/dump-${DATE}.sql.gz
 
-if [ $? -eq 0 ]; then
+  if [ $? -eq 0 ]; then
     echo "backup created: ${DATE}"
-else
+  else
     echo "backup not successful: ${DATE}"
     exit 1
-fi
+  fi
 
-# Delete old files
-old_dumps=$(ls -1 $BACKUP_DATA_DIR/dump* | head -n -$BACKUP_KEEP)
-if [ "$old_dumps" ]; then
+  # Delete old files
+  old_dumps=$(ls -1 $BACKUP_DIR/dump* | head -n -$BACKUP_KEEP)
+  if [ "$old_dumps" ]; then
     echo "Deleting: $old_dumps"
     rm $old_dumps
-fi
+  fi
+}
+
+databases=`mysql --user=$MYSQL_ROOT_USER --password=$MYSQL_ROOT_PASSWORD --host=$MYSQL_SERVICE_HOST -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema)"`
+
+for db in $databases; do
+  backup_db $db
+done
